ef70562310b0f3ccae726618b5d1da02143a3328
jasonliao1011-droid <jasonliao1011@gmail.com>
2026-09-25 20:47:07 +0800

    Bump dashboard service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bedside-dashboard-v3';
+const CACHE='bedside-dashboard-v4';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
